c9f2ce8965dd73012c86956967b3797d55284e71
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-09-27 23:34:09 +0530

    Added closures

--- JS Advance Objects and Functions/script.js
+++ JS Advance Objects and Functions/script.js
@@ -229,3 +229,76 @@ game();
 // checking data privacy 
 //console.log(score);
 */
+
+
+
+/*********************************
+ *  Closures
+*/
+
+
+// Generic function
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2020 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+
+// Specific function
+var retirementUs = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUs(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+//retirement(66)(1990);
+//retirement(65)(1990);
+//retirement(67)(1990);
+
+/*
+CLOSURE:
+An inner function has always access to the varaiables and parameters of its outer function, even after the outer function has returned.
+*/
+
+/*
+function interviewQuestion(job) {
+    if(job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is ?');
+        }
+    } else if(job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach ' + name + ' ?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do ?');
+        }
+    }
+}
+*/
+
+
+// Above function using closures
+function interviewQuestion(job) {
+    return function(name) {
+        if(job === 'designer') {
+            console.log(name + ', can you please explain what UX design is ?');
+        } else if(job === 'teacher') {
+            console.log('What subject do you teach ' + name + ' ?');
+        } else {
+            console.log('Hello ' + name + ', what do you do ?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('john');
+
+/*
+The difference between the two functions is that now, the decision is taken right inside of the function that we return, and that's possible because we can use the job variable to take this decision here, even after the interviewQuestion function has already returned.
+*/
