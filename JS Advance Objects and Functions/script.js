843de0f484ff103e67fff668447470fd2cd95a9c
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-09-28 23:07:31 +0530

    Added javascript console quiz game v2

--- JS Advance Objects and Functions/script.js
+++ JS Advance Objects and Functions/script.js
@@ -370,6 +370,7 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+/*
 (
     function() {
         function Question(question, answers, correct) {
@@ -409,5 +410,92 @@ c) correct answer (I would use a number for this)
         var answer = parseInt(prompt('Please select the correct answer.'));
         
         questions[randomQuestion].checkAnswer(answer);
+    }
+)();
+*/
+
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score.
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(
+    function() {
+        function Question(question, answers, correct) {
+            this.question = question;
+            this.answers = answers;
+            this.correct = correct;
+        }
+        
+        Question.prototype.displayQuestion = function() {
+            console.log(this.question);
+        
+            for(var i = 0; i < this.answers.length; i++) {
+                console.log(i + ': ' + this.answers[i]);
+            }
+        }
+
+        var sc;
+        
+        Question.prototype.checkAnswer = function(ans, callback) {
+            if(ans === this.correct) {
+                console.log('Correct answer!');
+                sc = callback(true);
+            } else {
+                console.log('Wrong answer! Try again :)');
+                sc = callback(false);
+            }
+
+            this.displayScore(sc);
+        }
+
+        Question.prototype.displayScore = function(score) {
+            console.log('Your current score is: ' + score);
+            console.log('---------------------------------------');
+        }
+        
+        var q1 = new Question('Do you enjoy programming?', ['yes', 'no'], 0);
+        
+        var q2 = new Question('Which is your favourite programming language?', ['Javascript', 'Python', 'C', 'C++'], 0);
+        
+        var q3 = new Question('What describes programming best?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+        var questions = [q1, q2, q3];
+
+        function score() {
+            var sc = 0;
+            return function(correct) {
+                if(correct) {
+                    sc++;
+                } 
+                return sc;
+            }
+        }
+
+        var keepScore = score();
+
+        function nextQuestion() {        
+            var randomQuestion = Math.floor(Math.random() * questions.length);
+        
+            questions[randomQuestion].displayQuestion();
+        
+            var answer = prompt('Please select the correct answer.');      
+        
+            if(answer !== 'exit') {
+                questions[randomQuestion].checkAnswer(parseInt(answer), keepScore);
+
+                nextQuestion();
+            }        
+        }
+
+        nextQuestion();
     }
 )();
