52e69709f6480649344adf659586f322e8e16ed8
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-09-28 17:49:48 +0530

    Added javascript console quiz game v1

--- JS Advance Objects and Functions/script.js
+++ JS Advance Objects and Functions/script.js
@@ -308,7 +308,7 @@ The difference between the two functions is that now, the decision is taken righ
 /***************************************
  * Bind, call and apply
  */
-
+/*
 var john = {
     name: 'John',
     age: 20,
@@ -341,7 +341,73 @@ john.presentation.apply(emily, ['formal', 'evening'])
 var johnFriendly = john.presentation.bind(john, 'friendly');
 johnFriendly('morning');
 johnFriendly('night');
+*/
 
 /*
 Bind method is similar to call function. The difference is tha t the bind function does not immediately call the fucntion. But instead it generates the copy of the function so that we can store it somewhere.
 */
+
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+(
+    function() {
+        function Question(question, answers, correct) {
+            this.question = question;
+            this.answers = answers;
+            this.correct = correct;
+        }
+        
+        Question.prototype.displayQuestion = function() {
+            console.log(this.question);
+        
+            for(var i = 0; i < this.answers.length; i++) {
+                console.log(i + ': ' + this.answers[i]);
+            }
+        }
+        
+        Question.prototype.checkAnswer = function(ans) {
+            if(ans === this.correct) {
+                console.log('Correct answer!');
+            } else {
+                console.log('Wrong answer! Try again.');
+            }
+        }
+        
+        var q1 = new Question('Do you enjoy programming?', ['yes', 'no'], 0);
+        
+        var q2 = new Question('Which is your favourite programming language?', ['Javascript', 'Python', 'C', 'C++'], 1);
+        
+        var q3 = new Question('What describes programming best?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+        
+        var questions = [q1, q2, q3];
+        
+        var randomQuestion = Math.floor(Math.random() * questions.length);
+        
+        questions[randomQuestion].displayQuestion();
+        
+        var answer = parseInt(prompt('Please select the correct answer.'));
+        
+        questions[randomQuestion].checkAnswer(answer);
+    }
+)();
