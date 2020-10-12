4f4ac095b92cd744322da10b963d913e65e55c55
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-10-12 19:34:39 +0530

    Strings in ES6 / ES2015.

--- JS ES6/script.js
+++ JS ES6/script.js
@@ -84,3 +84,29 @@ console.log(c);
 
 //console.log(c);
 */
+
+
+/*********************
+* Strings in ES6 
+*/
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2020 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`); // tempelate literals
+
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'), n.startsWith('j'));
+console.log(n.endsWith('Sm'));
+console.log(n.includes(' '));
+console.log(`${firstName} `.repeat(5));
